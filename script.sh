cb39f43a58c70915a85aaf139bd90b73e14c522a
Ubuntu <ubuntu@ip-172-31-35-95.eu-north-1.compute.internal>
2024-11-14 21:53:13 +0000

    First commit

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+######################################
+#Author: Abishek
+#Date: 15.10.2024
+#Reason: Find node Health
+#Version: v1
+######################################
+
+
+set -x #debug mode
+df -h #Disk space
+nproc #CPU space
+free -g #RAM space
